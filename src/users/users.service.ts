87588ac2f1cb88a05f2d0f506c60a163e4d22d7d
meriem <meriembenismail@insat.u-carthage.tn>
2022-02-16 19:36:35 -0800

    init

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from "@nestjs/common";
+import { v4 as uuidv4 } from 'uuid';
+import { UpdateUserDto } from "./dto/update-user.dto";
+
+import { User } from "./schemas/user.schema";
+import { UsersRepository } from "./users.repository";
+
+@Injectable()
+export class UsersService {
+    constructor(private readonly usersRepository: UsersRepository) {}
+
+    async getUserById(userId: string): Promise<User> {
+        return this.usersRepository.findOne({ userId })
+    }
+
+    async getUsers(): Promise<User[]> {
+        return this.usersRepository.find({});
+    }
+
+    async createUser(name: string): Promise<User> {
+        return this.usersRepository.create({
+            id: uuidv4(),
+            name
+        })
+    }
+
+    async updateUser(userId: string, userUpdates: UpdateUserDto): Promise<User> {
+        return this.usersRepository.findOneAndUpdate({ userId }, userUpdates);
+    }
+}
